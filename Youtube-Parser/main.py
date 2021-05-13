31f0d5eed870a542e1ca4acb0c78c1a2d83f1885
Brandon BORGES <37338331+BranditoFr@users.noreply.github.com>
2021-05-13 19:13:55 +0200

    Add files via upload

--- Youtube-Parser/main.py
+++ Youtube-Parser/main.py
@@ -1,68 +1,108 @@
-from googleapiclient.discovery import build
+##  pip install google-api-python-client
 import pandas as pd
+import numpy as np
+import fastparquet
+import datetime
+import os
+from fastparquet import write
+from datetime import date
+from googleapiclient.discovery import build
+from googleapiclient import discovery, errors
+from pprint import pprint
 
-youTubeApiKey = "AIzaSyDFF2AjGVG1WwZW8uCgo5yQtr16-rrv5aQ"
-youtube = build('youtube','v3', developerKey=youTubeApiKey)
-channelId = "UCNtD7oDVld6YeaYV7tCXo1A"
+## Globals variables
+listToDF        = []
+suscriberCount  = []
+channel         = []
+title           = []
+datetime        = []
+liked           = []
+disliked        = []
+views           = []
+url             = []
+comment         = []
+inPathArtists   = r"C:\Users\brand\Desktop\Projets perso\Projet music analytics\listArtist.csv"
+outPathParquet  = "C:/Users/brand/Desktop/Projets perso/Projet music analytics/youtubeParser/"
+youTubeApiKey   = "AIzaSyDFF2AjGVG1WwZW8uCgo5yQtr16-rrv5aQ"
+youtube         = build('youtube','v3', developerKey=youTubeApiKey)
 
-statdata=youtube.channels().list(part='statistics',id=channelId).execute()
-stats=statdata['items'][0]['statistics']
-## Get count of subscribers in suscriberCount variable
-suscriberCount=stats['subscriberCount']
-suscriberCount
+## Extract artist and channel/username from csv
+data            = pd.read_csv(inPathArtists,sep=';')
+dfFromCsv       = pd.DataFrame(data, columns= ['artist','channel','username'])
 
-snippetdata=youtube.channels().list(part='snippet',id=channelId).execute()
-## Get the channel name in channel variable
-channel=snippetdata['items'][0]['snippet']['title']
-channel
+## Loop for iterate on each artist
+for index,row in dfFromCsv.iterrows():
+    if row['username'] != "Null":
+        statdata    = youtube.channels().list(part='statistics',forUsername=row['username']).execute()
+        snippetdata = youtube.channels().list(part='snippet', forUsername=row['username']).execute()
+        contentdata = youtube.channels().list(part='contentDetails',forUsername=row['username']).execute()
+    else:
+        statdata    = youtube.channels().list(part='statistics',id=row['channel']).execute()
+        snippetdata = youtube.channels().list(part='snippet',id=row['channel']).execute()
+        contentdata = youtube.channels().list(part='contentDetails', id=row['channel']).execute()
 
-## Get All the video details in variable video
-contentdata=youtube.channels().list(id=channelId,part='contentDetails').execute()
-playlist_id = contentdata['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-videos = []
-next_page_token = None
+    ## Get total subscriber
+    statist         = statdata['items'][0]['statistics']
 
-while 1:
-    res = youtube.playlistItems().list(playlistId=playlist_id,
-                                    part='snippet',
-                                    maxResults=20,
-                                    pageToken=next_page_token).execute()
-    videos += res['items']
-    next_page_token = res.get('nextPageToken')
+    ## Get All the video details in variable video
+    playlist_id     = contentdata['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    videos          = []
+    next_page_token = None
 
-    if next_page_token is None:
-        break
+    while 1:
+        ## try catch for artist who don't have relatedplaylists
+        try :
+            res             = youtube.playlistItems().list(playlistId=playlist_id,
+                                           part='snippet',
+                                           maxResults=50,
+                                           pageToken=next_page_token).execute()
+            videos          += res['items']
+            next_page_token  = res.get('nextPageToken')
 
-video_ids = list(map(lambda x:x['snippet']['resourceId']['videoId'], videos))
-stats = []
-for i in range(0, len(video_ids), 40):
-    res = (youtube).videos().list(id=','.join(video_ids[i:i+40]),part='statistics').execute()
-    stats += res['items']
+            if next_page_token is None:
+                break
 
+        except errors.HttpError:
+            pprint("Erreur : L'artiste "+row['artist']+"  n'a pas de playlist associée.")
+            break
 
+    ##Get lists of videos ids
+    video_ids   = list(map(lambda x: x['snippet']['resourceId']['videoId'], videos))
+    ##Get stats of artists
+    stats       = []
 
-title=[]
-datetime=[]
-liked=[]
-disliked=[]
-views=[]
-##url=[]
-comment=[]
-nbVideos = 10
-if len(videos) < 10 :
-    nbVideos = len(videos)
-else:
+    for i in range(0, len(video_ids), 40):
+        res     = (youtube).videos().list(id=','.join(video_ids[i:i + 40]), part='statistics').execute()
+        stats   += res['items']
+    nbVideos    = 10
+
+    ##Get content of each videos
+    if len(videos) < nbVideos:
+        nbVideos = len(videos)
+
     for i in range(nbVideos):
+        ## Get count of subscribers in suscriberCount variable
+        suscriberCount.append(statist['subscriberCount'])
+        ## Get the channel name in channel variable
+        channel.append(snippetdata['items'][0]['snippet']['title'])
         title.append((videos[i])['snippet']['title'])
         datetime.append((videos[i])['snippet']['publishedAt'])
-        ##url.append("https://www.youtube.com/watch?v="+(videos[i])['snippet']['resourceId']['videoId'])
+        url.append("https://www.youtube.com/watch?v="+(videos[i])['snippet']['resourceId']['videoId'])
         liked.append(int((stats[i])['statistics']['likeCount']))
         disliked.append(int((stats[i])['statistics']['dislikeCount']))
         views.append(int((stats[i])['statistics']['viewCount']))
         comment.append(int((stats[i])['statistics']['commentCount']))
 
+data    = {'channel': channel, 'title': title, 'datetime': datetime, 'liked': liked, 'disliked': disliked,'views': views, 'comment': comment, 'followers': suscriberCount,'url':url}
+df      = pd.DataFrame(data)
+print(df.to_string())
 
-data = {'channel': channel, 'title': title, 'datetime': datetime, 'liked': liked, 'disliked': disliked,
-            'views': views, 'comment': comment, 'followers': suscriberCount}
-df = pd.DataFrame(data)
-print(df)
+
+##Create file parquet
+today       = date.today()
+path        = os.path.join(outPathParquet, today.strftime('%d%m%Y'))
+os.mkdir(path)
+write(today.strftime('%d%m%Y')+"/"+today.strftime('%d%m%Y')+'_youtube_artists.parquet', df)
+
+
+

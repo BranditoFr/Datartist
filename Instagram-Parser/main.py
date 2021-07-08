5a1c1948bfd6b218e4d3d9e48c65bb5c64f88fae
antoine <antoine.decrouez@yahoo.fr>
2021-07-08 17:36:44 +0200

    commit insta 08/07

--- Instagram-Parser/main.py
+++ Instagram-Parser/main.py
@@ -1,15 +1,28 @@
 import tst
+from datetime import datetime
+
 import pandas as pd
 import selenium
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time, urllib.request
 import requests
+from pyspark.sql import SparkSession
 
-driver = webdriver.Chrome(r'C:\Users\antoi\Documents\chromedriver.exe')
 
-csvFile = ["booba", "koba la d"]
+# spark = SparkSession.builder.appName("appName").getOrCreate()
+# sc = spark.sparkContext
+
+chromeOptions = Options()
+chromeOptions.headless = True
+driver = webdriver.Chrome(r'C:\Users\antoi\Documents\chromedriver.exe', options=chromeOptions)
+
+def current_time():
+    return datetime.now().strftime('%Y-%m-%d')
+
+csvFile = ["koba la d"]
 
 #, "vald", "niska", "ash kidd", "hamza", "ateyaba", "djadja&dinaz", "PNL","gazo", "nekfeu", "nadirmusic", "jok'air", "kaaris", "kalash criminel", "jul", "naps", "jsx"
 for artist in csvFile :
@@ -17,15 +30,16 @@ for artist in csvFile :
     tst.connect(artist)
 
     time.sleep(5)
-    tst.first_picture()
-    tst.like()
+    tst.first_picture(artist)
 
     tst.continue_like(artist)
 
     data = { 'artist':tst.artists, 'likes':tst.likes, 'followers':tst.followers_list}
 
-
+print(data)
+print(len(data))
 finalData = pd.DataFrame(data)
 
+finalData.to_csv(r'data/'+current_time()+'.csv')
 print(finalData)
 
--- Instagram-Parser/tst.py
+++ Instagram-Parser/tst.py
@@ -1,4 +1,8 @@
 import selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
@@ -7,14 +11,16 @@ import time, urllib.request
 import requests
 
 PATH = r'C:\Users\antoi\Documents\chromedriver.exe'
-options = Options()
-options.add_argument("start-maximized")
-driver = webdriver.Chrome(chrome_options=options, executable_path=r'C:\Users\antoi\Documents\chromedriver.exe' )
+# chromeOptions = Options()
+# chromeOptions.headless = True
+driver = webdriver.Chrome(r'C:\Users\antoi\Documents\chromedriver.exe')#, options=chromeOptions)
 
 likes = []
 followers_list = []
 artists = []
-
+date = []
+def current_time():
+    return datetime.now().strftime('%Y-%m-%d')
 
 def connect(name):
 
@@ -27,16 +33,16 @@ def connect(name):
         pass
 
     time.sleep(3)
-    try:
-        username = driver.find_element_by_css_selector("input[name='username']")
-        password = driver.find_element_by_css_selector("input[name='password']")
-        username.clear()
-        password.clear()
-        username.send_keys("FindYourTruck")
-        password.send_keys("esgi2018")
-        login = driver.find_element_by_css_selector("button[type='submit']").click()
-    except NoSuchElementException:
-        pass
+
+
+    username = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+    password = driver.find_element_by_css_selector("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+    username.clear()
+    password.clear()
+    username.send_keys("FindYourTruck")
+    password.send_keys("esgi2018")
+    login = driver.find_element_by_css_selector("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button").click()
+
     time.sleep(5)
     try:
         notnow = driver.find_element_by_xpath("//button[contains(text(), 'Plus tard')]").click()
@@ -49,26 +55,41 @@ def connect(name):
         pass
 
     time.sleep(5)
-    searchbox = driver.find_element_by_css_selector("input[placeholder='Rechercher']")
+    searchbox = driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")
+    time.sleep(1)
     searchbox.clear()
+    time.sleep(1)
     searchbox.send_keys(name)
     time.sleep(5)
     searchbox.send_keys(Keys.ENTER)
     time.sleep(3)
     searchbox.send_keys(Keys.ENTER)
     time.sleep(3)
-    followers = driver.find_element_by_xpath("/html/body/div[1]/div/div/section/main/div/header/section/ul/li[2]/a/span").text
+    followers = driver.find_element_by_xpath("/html/body/div[1]/div/div/section/main/div/header/section/ul/li[2]/a/span").get_attribute("title")
 
     i = 0
-    while(i < 2):
+    while(i < 4):
         followers_list.append(followers)
+        date.append(current_time())
+        i+=1
     time.sleep(2)
 
 
-def first_picture():
+def first_picture(artist):
     # finds the first picture
+    artists.append(artist)
+
     pic = driver.find_element_by_class_name('_9AhH0')
     pic.click()
+    exit = like()
+    while (exit != 1 ):
+        next_el = next_picture()
+        time.sleep(2)
+        if next_el != False:
+            next_el.click()
+            exit = like()
+        else:
+            break
 
 
 def like():
@@ -76,8 +97,10 @@ def like():
     try:
         like = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[2]/div/div/a/span').text
         likes.append(like)
+        return 1
         time.sleep(2)
     except NoSuchElementException:
+        return 0
         pass
 
 
@@ -95,20 +118,23 @@ def next_picture():
 
 def continue_like(artist):
     i = 0
-    while (i < 2):
+    while (i < 3):
         next_el = next_picture()
 
         # if next button is there then
         if next_el != False:
-            artists.append(artist)
+
             # click the next button
             next_el.click()
             time.sleep(2)
 
             # like the picture
-            like()
+            exit = like()
             time.sleep(2)
-            i+=1
+            if exit == 1:
+                artists.append(artist)
+                i+=1
+
         else:
             print("not found")
             break
